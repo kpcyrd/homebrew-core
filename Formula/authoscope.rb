35632edf8c049a21a1b7ad013ea0b7613f818b26
kpcyrd <git@rxv.cc>
2021-05-27 19:40:31 +0200

    authoscope: renamed from badtouch

--- Formula/authoscope.rb
+++ Formula/authoscope.rb
@@ -1,8 +1,8 @@
-class Badtouch < Formula
+class Authoscope < Formula
   desc "Scriptable network authentication cracker"
-  homepage "https://github.com/kpcyrd/badtouch"
-  url "https://github.com/kpcyrd/badtouch/archive/v0.7.3.tar.gz"
-  sha256 "cb09ae83bbe37e17ce1d5e86ba1c9f8b8dd1dc1b30e3f7d69d7cfd8db7ae5547"
+  homepage "https://github.com/kpcyrd/authoscope"
+  url "https://github.com/kpcyrd/authoscope/archive/v0.8.0.tar.gz"
+  sha256 "977df6f08a2fece7076f362bc9db6686b829de93ed3c29806d7b841a50bd9d1c"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -23,7 +23,14 @@ class Badtouch < Formula
     ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
 
     system "cargo", "install", *std_cargo_args
-    man1.install "docs/badtouch.1"
+    man1.install "docs/authoscope.1"
+
+    bash_output = Utils.safe_popen_read(bin/"authoscope", "completions", "bash")
+    (bash_completion/"authoscope").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"authoscope", "completions", "zsh")
+    (zsh_completion/"_authoscope").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"authoscope", "completions", "fish")
+    (fish_completion/"authoscope.fish").write fish_output
   end
 
   test do
@@ -34,6 +41,6 @@ class Badtouch < Formula
           return true
       end
     EOS
-    system "#{bin}/badtouch", "oneshot", "-vvx", testpath/"true.lua", "foo"
+    system "#{bin}/authoscope", "run", "-vvx", testpath/"true.lua", "foo"
   end
 end
